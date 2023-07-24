8eafd6016fba275d5aa39c9ebee10cb7f50d06da
aman <amansagar262@gmail.com>
2023-07-24 17:43:48 +0530

    chore: add firebase config

---
+++ src/firebase/index.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { getApps, initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "falsefriends-2f64b.firebaseapp.com",
+  projectId: "falsefriends-2f64b",
+  storageBucket: "falsefriends-2f64b.appspot.com",
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
+
+const APP_NAME = "falseFriends";
+
+export const app =
+  getApps().find((app) => app.name === APP_NAME) ??
+  initializeApp(firebaseConfig, APP_NAME);
